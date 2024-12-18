bd136eedb9670a9c0f77641aafac62d8781956fa
johnson-magic <2460171714@qq.com>
2024-12-18 15:39:30 +0800

    [FIX]: fix encode error

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,13 +1,22 @@
 #include<onnxruntime_cxx_api.h>
 #include <opencv2/opencv.hpp>
 #include <iostream>
+#include <filesystem>
+#include <chrono>
+#include <fstream>
 #include <vector>
 #include <math.h>
 #include <thread>
-#include <chrono> // 包含时间相关的库
+#include <chrono>
+#include <windows.h>
 
 using namespace cv;
 using namespace std;
+// namespace fs = std::filesystem;
+
+volatile bool keepRunning = true;
+
+
 #define pi acos(-1)
 float modelScoreThreshold=0.2;
 float modelNMSThreshold=0.8;
@@ -25,21 +34,19 @@ cv::Mat formatToSquare(const cv::Mat &source)
 }
 
 void drawRotatedRect(cv::Mat& image, const cv::RotatedRect& rotatedRect) {
-    // 获取 RotatedRect 的角点
+
     Point2f vertices[4];
-	RotatedRect rRect = RotatedRect(Point2f(100,100), Size2f(100,50), 30);
-	rRect.points(vertices);
+	
 
 
-    //rotatedRect.points(vertices);
+    rotatedRect.points(vertices);
 
-    //在图像上绘制旋转矩形的四个角点
-    // for(int j = 0; j < 4; ++j) {
-	// 	std::cout<<"123"<<std::endl;
-	// }
-        // 将角点连接成一个多边形
-        //cv::line(image, rotatedRect.points[0], rotatedRect.points[(0 + 1) % 4], cv::Scalar(0, 255, 0), 2);
-    //}
+   
+    for(int i = 0; i < 4; ++i) {
+		cv::line(image, vertices[i], vertices[(i + 1) % 4], cv::Scalar(0, 255, 0), 2);
+	}
+      
+        
 }
 
 // define a struct to save some information
@@ -50,9 +57,9 @@ typedef struct {
 }RotatedBOX;
 
 void printRotatedRect(const cv::RotatedRect& rotatedRect) {
-    cv::Point2f center = rotatedRect.center; // 矩形中心
-    cv::Size2f size = rotatedRect.size;       // 矩形的宽和高
-    float angle = rotatedRect.angle;          // 旋转角度
+    cv::Point2f center = rotatedRect.center; 
+    cv::Size2f size = rotatedRect.size;       
+    float angle = rotatedRect.angle;         
 
     std::cout << "RotatedRect:" << std::endl;
     std::cout << "Center: (" << center.x << ", " << center.y << ")" << std::endl;
@@ -60,38 +67,57 @@ void printRotatedRect(const cv::RotatedRect& rotatedRect) {
     std::cout << "Angle: " << angle << " degrees" << std::endl;
 }
 
-int main(){
-    std::string imagePath = "A-2024-01-03-14-13-09_000032.jpg";
-    std::string onnx_path_name = "best-cpu.onnx";
 
-
-    cv::Mat image_src = cv::imread(imagePath);
-    if (image_src.empty()) {
-        std::cerr << "Failed to read the image!" << std::endl;
-        return -1;
+bool hasImageUpdated(const std::string& imagePath, std::filesystem::file_time_type& lastCheckedTime) {
+    
+    if (!std::filesystem::exists(imagePath)) {
+        std::cout << "file does not exists: " << imagePath << std::endl;
+        return false;
     }
-    cv::Mat image = formatToSquare(image_src);
 
-    std::vector<std::string> input_node_names;
-	std::vector<std::string> output_node_names;
 
-    Ort::SessionOptions session_options;
-	// std::string model_name = "yolov11-onnx";
-	// see https://github.com/ami-iit/bipedal-locomotion-framework/commit/77a3313d2e5ab385fbc1c7dc1ff89681dd723def#diff-ebc4740cd40eee3ebb9bfc92a49ed08565c040e255df15d0e8e5ec5fe138c4daR173
-	// std::basic_string<ORTCHAR_T> model_name_w(model_name.begin(), model_name.end());
+    std::filesystem::file_time_type curWriteTime = std::filesystem::last_write_time(imagePath);
+    
+    if (curWriteTime != lastCheckedTime) {
+        lastCheckedTime = curWriteTime;
+        return true;
+    }
+    
+    return false;
+}
+
+
+BOOL WINAPI HandleCtrlC(DWORD signal) {
+    if (signal == CTRL_C_EVENT) {
+        keepRunning = false;
+    }
+    return TRUE;
+}
+
+int main(){
+	std::cout<<"00000000000000000"<<std::endl;
+
+	std::string imagePath = "A-2024-01-03-14-13-09_000032.jpg";
+    std::string onnx_path_name = "best.onnx";
+
+	Ort::SessionOptions session_options;
 	Ort::Env env = Ort::Env(ORT_LOGGING_LEVEL_ERROR, "yolov11-onnx");
 	session_options.SetGraphOptimizationLevel(ORT_ENABLE_BASIC);
-	
+	std::cout<<"010000000000000000"<<std::endl;
 	std::basic_string<ORTCHAR_T> model_path_w(onnx_path_name.begin(), onnx_path_name.end());
+	std::cout<<"020000000000000000"<<std::endl;
 	Ort::Session session_(env, model_path_w.c_str(), session_options);
-
+	std::vector<std::string> input_node_names;
+	std::vector<std::string> output_node_names;
+	std::cout<<"030000000000000000"<<std::endl;
     size_t numInputNodes = session_.GetInputCount();
 	size_t numOutputNodes = session_.GetOutputCount();
 	Ort::AllocatorWithDefaultOptions allocator;
 	input_node_names.reserve(numInputNodes);
-	std::cout<<"111111111111111111111111111111"<<std::endl;
 
-    // get the input information
+	std::cout<<"1111111111111111111"<<std::endl;
+
+	// get the input information
 	int input_w = 0;
 	int input_h = 0;
 	for (int i = 0; i < numInputNodes; i++) {
@@ -102,10 +128,10 @@ int main(){
 		auto input_dims = input_tensor_info.GetShape();
 		input_w = input_dims[3];
 		input_h = input_dims[2];
-		//std::cout << "input format: NxCxHxW = " << input_dims[0] << "x" << input_dims[1] << "x" << input_dims[2] << "x" << input_dims[3] << std::endl;
+		
 	}
 
-    // get the output information
+	// get the output information
 	int output_h = 0;
 	int output_w = 0;
 	Ort::TypeInfo output_type_info = session_.GetOutputTypeInfo(0);
@@ -119,128 +145,137 @@ int main(){
 		auto out_name = session_.GetOutputNameAllocated(i, allocator);
 		output_node_names.push_back(out_name.get());
 	}
-	//std::cout << "input: " << input_node_names[0] << " output: " << output_node_names[0] << std::endl;
+	std::cout<<"222222222222222222"<<std::endl;
+	std::filesystem::file_time_type lastCheckedTime = std::filesystem::file_time_type();
+	
+	while (keepRunning) {
+        if (hasImageUpdated(imagePath, lastCheckedTime)) {
 
-	std::cout<<"2222222222222222222222222222222"<<std::endl;
-    float x_factor = image.cols / static_cast<float>(input_w);
-	float y_factor = image.rows / static_cast<float>(input_h);
+			cv::Mat image_src = cv::imread(imagePath);
+    		if (image_src.empty()) {
+        		std::cerr << "Failed to read the image!" << std::endl;
+        		break;
+    		}
+    		cv::Mat image = formatToSquare(image_src);
 
-	cv::Mat blob;
-    cv::dnn::blobFromImage(image,blob, 1 / 255.0, cv::Size(input_w, input_h), cv::Scalar(0, 0, 0), true, false);
-	size_t tpixels = input_h * input_w * 3;
-	std::array<int64_t, 4> input_shape_info{ 1, 3, input_h, input_w };
+			float x_factor = image.cols / static_cast<float>(input_w);
+			float y_factor = image.rows / static_cast<float>(input_h);
+			std::cout<<"33333333333333333333333"<<";"<<input_w<<";"<<input_h<<std::endl;
+			cv::Mat blob;
+			cv::dnn::blobFromImage(image,blob, 1 / 255.0, cv::Size(input_w, input_h), cv::Scalar(0, 0, 0), true, false);
+			size_t tpixels = input_h * input_w * 3;
+			std::array<int64_t, 4> input_shape_info{ 1, 3, input_h, input_w };
+			std::cout<<"44444444444444444444444"<<std::endl;
 
-	// set input data and inference
-	auto allocator_info = Ort::MemoryInfo::CreateCpu(OrtDeviceAllocator, OrtMemTypeCPU);
-	Ort::Value input_tensor_ = Ort::Value::CreateTensor<float>(allocator_info, blob.ptr<float>(), tpixels, input_shape_info.data(), input_shape_info.size());
-	const std::array<const char*, 1> inputNames = { input_node_names[0].c_str() };
-	const std::array<const char*, 1> outNames = { output_node_names[0].c_str() };
-	std::vector<Ort::Value> ort_outputs;
-	try {
-		ort_outputs = session_.Run(Ort::RunOptions{ nullptr }, inputNames.data(), &input_tensor_, 1, outNames.data(), outNames.size());
-	}
-	catch (std::exception e) {
-		std::cout << e.what() << std::endl;
-	}
-	std::cout<<"333333333333333333333333333333"<<std::endl;
-	// output data
-	const float* pdata = ort_outputs[0].GetTensorMutableData<float>();
-	cv::Mat dout(output_h, output_w, CV_32F, (float*)pdata);
-	cv::Mat det_output = dout.t(); // 8400x84
+			// set input data and inference
+			auto allocator_info = Ort::MemoryInfo::CreateCpu(OrtDeviceAllocator, OrtMemTypeCPU);
+			Ort::Value input_tensor_ = Ort::Value::CreateTensor<float>(allocator_info, blob.ptr<float>(), tpixels, input_shape_info.data(), input_shape_info.size());
+			const std::array<const char*, 1> inputNames = { input_node_names[0].c_str() };
+			const std::array<const char*, 1> outNames = { output_node_names[0].c_str() };
+			std::vector<Ort::Value> ort_outputs;
+			try {
+				ort_outputs = session_.Run(Ort::RunOptions{ nullptr }, inputNames.data(), &input_tensor_, 1, outNames.data(), outNames.size());
+			}
+			catch (std::exception e) {
+				std::cout << e.what() << std::endl;
+			}
 
-    // post-process
-	std::vector<cv::RotatedRect> boxes;
-    std::vector<cv::RotatedRect> boxes_fenleis;
-	std::vector<float> confidences;
-    std::vector<RotatedBOX>BOXES;
-    std::vector<int>class_list;
 
-	for (int i = 0; i < det_output.rows; i++) {
-		cv::Mat classes_scores = det_output.row(i).colRange(4, 4+labels.size());
-		cv::Point classIdPoint;
-		double score;
-		minMaxLoc(classes_scores, 0, &score, 0, &classIdPoint);
-        RotatedBOX BOX;
+			// output data
+			const float* pdata = ort_outputs[0].GetTensorMutableData<float>();
+			cv::Mat dout(output_h, output_w, CV_32F, (float*)pdata);
+			cv::Mat det_output = dout.t(); // 8400x84
 
-		// confidence between 0 and 1
-		if (score > modelScoreThreshold)
-		{
-			float cx = det_output.at<float>(i, 0)*x_factor;
-			float cy = det_output.at<float>(i, 1)* y_factor;
-			float ow = det_output.at<float>(i, 2)*x_factor;
-			float oh = det_output.at<float>(i, 3)* y_factor;
-            float angle=det_output.at<float>(i,det_output.cols-1);
-            //angle in [-pi/4,3/4 pi) --》 [-pi/2,pi/2)
-            if (angle>=0.5*pi && angle <= 0.75*pi)
-            {
-                angle=angle-pi;
-				//cout<<angle<<endl;
-            }
+			// post-process
+			std::vector<cv::RotatedRect> boxes;
+			std::vector<cv::RotatedRect> boxes_fenleis;
+			std::vector<float> confidences;
+			std::vector<RotatedBOX>BOXES;
+			std::vector<int>class_list;
+
+			for (int i = 0; i < det_output.rows; i++) {
+				cv::Mat classes_scores = det_output.row(i).colRange(4, 4+labels.size());
+				cv::Point classIdPoint;
+				double score;
+				minMaxLoc(classes_scores, 0, &score, 0, &classIdPoint);
+				RotatedBOX BOX;
+
+				// confidence between 0 and 1
+				if (score > modelScoreThreshold)
+				{
+					float cx = det_output.at<float>(i, 0)*x_factor;
+					float cy = det_output.at<float>(i, 1)* y_factor;
+					float ow = det_output.at<float>(i, 2)*x_factor;
+					float oh = det_output.at<float>(i, 3)* y_factor;
+					float angle=det_output.at<float>(i,det_output.cols-1);
+					//angle in [-pi/4,3/4 pi) --》 [-pi/2,pi/2)
+					if (angle>=0.5*pi && angle <= 0.75*pi)
+					{
+						angle=angle-pi;
+						//cout<<angle<<endl;
+					}
            
-            BOX.Classindex=classIdPoint.x;
-           
-		    class_list.push_back(classIdPoint.x);
-            BOX.score=score; 
-            cv::RotatedRect box=cv::RotatedRect(cv::Point2f(cx,cy),cv::Size2f(ow,oh),angle*180/pi);
-            cv::RotatedRect boxes_fenlei=cv::RotatedRect(cv::Point2f(cx+10000*BOX.Classindex,cy),cv::Size2f(ow,oh),angle*180/pi);
-            BOX.box=box;
-            boxes.push_back(box);
-            boxes_fenleis.push_back(boxes_fenlei);
-            BOXES.push_back(BOX); 
-			confidences.push_back(score);
-		}
-	}
+					BOX.Classindex=classIdPoint.x;
+				
+					class_list.push_back(classIdPoint.x);
+					BOX.score=score; 
+					cv::RotatedRect box=cv::RotatedRect(cv::Point2f(cx,cy),cv::Size2f(ow,oh),angle*180/pi);
+					cv::RotatedRect boxes_fenlei=cv::RotatedRect(cv::Point2f(cx+10000*BOX.Classindex,cy),cv::Size2f(ow,oh),angle*180/pi);
+					BOX.box=box;
+					boxes.push_back(box);
+					boxes_fenleis.push_back(boxes_fenlei);
+					BOXES.push_back(BOX); 
+					confidences.push_back(score);
+				}
+			}
 
 
-     // NMS accoding to each class
+			// NMS accoding to each class
+			std::cout<<"before nms"<<boxes.size()<<std::endl;
+			std::vector<RotatedBOX> Remain_boxes;
+			std::vector<int> nms_result;
+			cv::dnn::NMSBoxes(boxes_fenleis,confidences,modelScoreThreshold,modelNMSThreshold, nms_result);
+			std::cout<<modelScoreThreshold<<";"<<modelNMSThreshold<<std::endl;
 
-    // std::set<int>uniqueClass(class_list.begin(),class_list.end());
-    // std::vector<int>uniqueClass_(uniqueClass.begin(),uniqueClass.end());
-    std::cout<<"before nms"<<boxes.size()<<std::endl;
-    std::vector<RotatedBOX> Remain_boxes;
-	std::vector<int> nms_result;
-	cv::dnn::NMSBoxes(boxes_fenleis,confidences,modelScoreThreshold,modelNMSThreshold, nms_result);
-    std::cout<<modelScoreThreshold<<";"<<modelNMSThreshold<<std::endl;
 
-    // // for (int i=0;i< boxes.size();i++)
-	// // {
-    // //     if(i>1)
-    // //       break;
-    // //     drawRotatedRect(image_src, boxes[i]);
-    // //     std:cout<<i<<":"<<confidences[i]<<std::endl;
+			for (int i=0;i< nms_result.size();i++)
+			{
+				int index=nms_result[i];
+				RotatedBOX Box_=BOXES[index];
 
-	// // }
+				printRotatedRect(Box_.box);
+				drawRotatedRect(image_src, Box_.box);
 
-	for (int i=0;i< nms_result.size();i++)
-	{
-		int index=nms_result[i];
-		RotatedBOX Box_=BOXES[index];
+				Remain_boxes.push_back(Box_);
 
-        printRotatedRect(Box_.box);
-        drawRotatedRect(image_src, Box_.box);
+			}
 
-		Remain_boxes.push_back(Box_);
+			for (auto remain_box : Remain_boxes){
+				int cls_id = remain_box.Classindex;
 
-	}
-    cv::imwrite("output_image.jpg", image_src);  // 输出的图片名
+				float obj_score = remain_box.score;
+				if(obj_score < 0.7){
+					continue;
+				}
 
-    session_options.release();
-	session_.release();
-    
-    std::cout<<Remain_boxes.size()<<";"<<nms_result.size()<<std::endl;
-	std::cout << "程序将在1分钟后退出..." << std::endl;
-	std::this_thread::sleep_for(std::chrono::minutes(1));
-    return 0;
-    //return Remain_boxes;
+				std::cout<<labels[cls_id]<<"; score is:"<<obj_score<<"; coor is:";
+				cv::Point2f vertices[4];
+    			remain_box.box.points(vertices);
+				for(auto vertice : vertices){
+					std::cout<<vertice.x<<";"<<vertice.y<<std::endl;
+				}
+				std::cout<<std::endl;
+			}
+
+    		cv::imwrite("output_image.jpg", image_src);
+			std::cout<<Remain_boxes.size()<<";"<<nms_result.size()<<std::endl;
+        }
+    }
 
 	
-
+    session_options.release();
+	session_.release();
+	std::cout << "exit after 1 minitus..." << std::endl;
+	std::this_thread::sleep_for(std::chrono::minutes(1));
+    return 0;
 }
-
-// #include <iostream>
-// #include "opencv2/opencv.hpp"
-
-// int main(){
-// 	std::cout<<CV_VERSION<<std::endl;
-// 	return 0;
-// }
