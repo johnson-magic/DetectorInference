66439a793792cb16477f2407f0649161b39921d0
magic_zhang <putaoisapig@163.com>
2024-12-22 21:45:07 +0800

    [BACKEND]: update utils.h

--- cpp/utils.h
+++ cpp/utils.h
@@ -1,6 +1,7 @@
+#pragma once
+#include <filesystem>
 #include <opencv2/opencv.hpp>
 
 void drawRotatedRect(cv::Mat& image, const cv::RotatedRect& rotatedRect);
 void printRotatedRect(const cv::RotatedRect& rotatedRect);
 bool hasImageUpdated(const std::string& image_path, std::filesystem::file_time_type& lastCheckedTime);
-BOOL WINAPI HandleCtrlC(DWORD signal);
