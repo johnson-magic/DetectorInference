8dbfae05d2cca8913403869af8b479863eff7fc8
magic_zhang <putaoisapig@163.com>
2024-12-22 21:43:59 +0800

    [BACKEND]: update plugin.h

--- cpp/plugin.h
+++ cpp/plugin.h
@@ -1,21 +1,25 @@
+#pragma once
 #include <string>
+#include <fstream>
 #include <opencv2/opencv.hpp>
 
 #include "data_structure.h"
 #include "utils.h"
 
+
+
 class PrecisionAngleDetection{
-    PrecisionAngleDetection(std::string image_path_, std::string result_path, std::string vis_path): image_path_(image_path), result_path_(result_path), vis_path_(vis_path){
+
+public:
+    PrecisionAngleDetection(std::string image_path, std::string result_path, std::string vis_path): image_path_(image_path), result_path_(result_path), vis_path_(vis_path){
 
     };
 
 private:
-    /*以下变量和类的构建有关*/
     std::string image_path_;
     std::string result_path_;
     std::string vis_path_;
 
-    /*以下变量和业务需求有关*/
     cv::Point2f center_point_;
 	float diameter_;
 	float angle_;
