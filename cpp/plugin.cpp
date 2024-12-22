a5db3b00400d74f9ddb612ec15cabca852176463
magic_zhang <putaoisapig@163.com>
2024-12-22 21:43:25 +0800

    [BACKEND]: update plugin.cpp

--- cpp/plugin.cpp
+++ cpp/plugin.cpp
@@ -3,40 +3,40 @@
 
 void PrecisionAngleDetection::Process(const std::vector<RotatedObj> & rotated_objs){
 	for(auto rotated_obj : rotated_objs){
-		int cls_id = rotated_obj.Classindex;
+		int cls_id = rotated_obj.class_index;
 		float obj_score = rotated_obj.score;
 		if(obj_score < 0.7){
 			continue;
 		}
 
 		cv::Point2f vertices[4];
-		rotated_obj.box.points(vertices);  // clockwise
+		rotated_obj.rotated_rect.points(vertices);  // clockwise
 
 		if(0 == cls_id){
-			center_point_ = rotated_obj.box.center;
-			diameter_ = (rotated_obj.box.size.width + rotated_obj.box.size.height) / 2.0;
+			center_point_ = rotated_obj.rotated_rect.center;
+			diameter_ = (rotated_obj.rotated_rect.size.width + rotated_obj.rotated_rect.size.height) / 2.0;
 		}
 		else if(1 == cls_id){
-			if(rotated_obj.box.size.width >= rotated_obj.box.size.height){  // w >= h
-				angle_ = - rotated_obj.box.angle;
+			if(rotated_obj.rotated_rect.size.width >= rotated_obj.rotated_rect.size.height){  // w >= h
+				angle_ = - rotated_obj.rotated_rect.angle;
 			}
 			else{
-				angle_ = -(90 + rotated_obj.box.angle);
+				angle_ = -(90 + rotated_obj.rotated_rect.angle);
 			}
 		}
 		else if(2 == cls_id){
-			if(rotated_obj.box.size.width >= rotated_obj.box.size.height){
-				slider_angle_ = - rotated_obj.box.angle;
+			if(rotated_obj.rotated_rect.size.width >= rotated_obj.rotated_rect.size.height){
+				slider_angle_ = - rotated_obj.rotated_rect.angle;
 			}
 			else{
-				slider_angle_ = -(90 + rotated_obj.box.angle);
+				slider_angle_ = -(90 + rotated_obj.rotated_rect.angle);
 			}
-			slider_center_point_ = rotated_obj.box.center;
+			slider_center_point_ = rotated_obj.rotated_rect.center;
 		}
 
 	}
 
-	if(slider_center_point.y < slider_center_point.y){
+	if(slider_center_point_.y < slider_center_point_.y){
 		position_ = "above";
 	}
 	else{
@@ -46,13 +46,11 @@ void PrecisionAngleDetection::Process(const std::vector<RotatedObj> & rotated_ob
 }
 
 void PrecisionAngleDetection::VisRes(const std::vector<RotatedObj> & rotated_objs){
-	image = cv::imread(image_path_);
+	cv::Mat image = cv::imread(image_path_);
 	if (image.empty()) {
 		std::cerr << "Failed to read the image!" << std::endl;
-		break;
+		return;
 	}
-
-	cv::Mat& image, const cv::RotatedRect& rotatedRect
 
 	for(auto rotated_obj : rotated_objs){
 		cv::RotatedRect rotated_rect = rotated_obj.rotated_rect;
@@ -64,21 +62,18 @@ void PrecisionAngleDetection::VisRes(const std::vector<RotatedObj> & rotated_obj
 void PrecisionAngleDetection::SaveRes(){
 	
 	std::ofstream outFile(result_path_);
-	// 检查文件是否成功打开
 	if (!outFile) {
-		std::cerr << "can not open: " << result_path << std::endl;
-		return -1;
+		std::cerr << "can not open: " << result_path_ << std::endl;
+		return;
 	}
 
-	// 将变量写入文件
 	outFile << "Centerpoint: " << center_point_.x <<"," << center_point_.y << std::endl;
 	outFile << "angle: " << angle_ << std::endl;
 	outFile << "SliderAngle: " << slider_angle_ << std::endl;
 	outFile << "Diameter: " << diameter_ << std::endl;
 	outFile << "SliderCenterPoint: " << slider_center_point_.x << "," << slider_center_point_.y << std::endl;
-	outFile << "Position: " << position_ << std::endl;
+	outFile<<"Position: "<<position_<<std::endl;
 
-	// 关闭文件
 	outFile.close();
 }
 
